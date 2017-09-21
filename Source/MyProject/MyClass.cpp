3e367ef60105b9e994abfe3cbafee7b5ac8013fa
unknown <1302534@MC10810.uad.ac.uk>
2017-09-21 16:46:24 +0100

    final prototype level

---
+++ Source/MyProject/MyClass.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+
+
+void MyClass::Invert_Gravity() {
+
+};
---
+++ Source/MyProject/MyClass.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MYPROJECT_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+	UFUNCTION(BlueprintCallable, Category = "Physics")
+		void Invert_Gravity();
+};
